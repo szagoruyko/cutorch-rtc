ee1f27d7513484907c0b5674b860452c821d251d
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-06-11 21:06:33 +0200

    ptx moved to a separate file

--- apply.cu
+++ apply.cu
@@ -1,4 +1,3 @@
-#include <nvrtc.h>
 #include <vector>
 #include <memory>
 #include <unordered_map>
@@ -7,6 +6,8 @@
 
 #include "THC/THC.h"
 #include "THC/THCApply.cuh"
+
+#include "compile_ptx.h"
 
 struct Apply1Hash {
   std::string op;
@@ -110,73 +111,6 @@ Apply1Cache apply1cache;
 Apply2Cache apply2cache;
 Apply3Cache apply3cache;
 
-
-inline void NVRTC_CHECK(nvrtcResult result)
-{
-  if(result != NVRTC_SUCCESS)
-    THError(nvrtcGetErrorString(result));
-}
-
-void compilePTX(const char* src,
-    		const char* headers[],
-		const char* includeNames[],
-		std::vector<char>& ptx)
-{
-  nvrtcProgram program;
-  NVRTC_CHECK(nvrtcCreateProgram(&program, src, NULL, 1, headers, includeNames));
-
-  nvrtcResult result = nvrtcCompileProgram(program, 0, NULL); 
-  if(result == NVRTC_ERROR_COMPILATION)
-  {
-    size_t logsize;
-    nvrtcGetProgramLogSize(program, &logsize);
-
-    std::vector<char> log(logsize);
-    nvrtcGetProgramLog(program, log.data());
-    THError(log.data());
-  }
-  else
-    NVRTC_CHECK(result);
-
-  size_t ptx_size;
-  NVRTC_CHECK(nvrtcGetPTXSize(program, &ptx_size));
-  ptx.resize(ptx_size);
-  NVRTC_CHECK(nvrtcGetPTX(program, ptx.data()));
-  NVRTC_CHECK(nvrtcDestroyProgram(&program));
-}
-
-inline void CUDA_CHECK(CUresult result)
-{
-  if(result != CUDA_SUCCESS)
-  {
-    const char* errstr;
-    cuGetErrorString(result, &errstr);
-    THError(errstr);
-  }
-}
-
-void launch(const char* ptx, const char* name, void* args[], dim3 grid, dim3 block, CUstream stream)
-{
-  CUmodule module;
-  CUfunction func;
-
-  CUDA_CHECK(cuModuleLoadData(&module, ptx));
-  CUDA_CHECK(cuModuleGetFunction(&func, module, name));
-
-  CUDA_CHECK(cuLaunchKernel(func,
-                            grid.x, grid.y, grid.z,
-                            block.x, block.y, block.z,
-                            0, stream, args, NULL));
-
-  CUDA_CHECK(cuModuleUnload(module));
-}
-
-extern "C"
-void launchPTX(THCState* state, const char* ptx, const char* name, void* args[], int* grid, int* block)
-{
-  cudaStream_t stream = state->currentStream;
-  launch(ptx, name, args, dim3(grid[0], grid[1], grid[2]), dim3(block[0], block[1], block[2]), (CUstream)stream);
-}
 
 // Example op: 'x = y*2'
 const char* instanciate_apply1 = "                                      \n\
---
+++ compile_ptx.h
@@ -0,0 +1,8 @@
+#ifndef THCRTC_COMPILE_RTC_H
+#define THCRTC_COMPILE_RTC_H
+
+#include "THC/THC.h"
+
+void launch(const char* ptx, const char* name, void* args[], dim3 grid, dim3 block, CUstream stream);
+
+#endif // THCRTC_COMPILE_RTC_H
